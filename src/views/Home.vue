d6eb2e3bbcb13296d845786087a0c8a91f96e73e
王俊坤 <wjk@xiaoniren.cn>
2019-08-01 00:42:02 +0800

    项目基础搭建完成

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <test></test>
+  </div>
+</template>
+
+<script>
+import test from '@/components/test'
+export default {
+  components: {
+    test
+  }
+};
+</script>
+<style lang="scss" scoped>
+  div {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+  }
+</style>
